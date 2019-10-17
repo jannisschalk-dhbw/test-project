1f86d6dc0e149f4f010460a59edad4012743c9c4
jschalk <jsc@form-solutions.de>
2019-10-17 23:03:16 +0200

    enabeld importing html files

--- src/overview/overview.js
+++ src/overview/overview.js
@@ -1,17 +1,21 @@
 "use strict";
 
 import stylesheet from "./overview.css";
+import overview from "./overview.html";
 
 class Overview {
-    
+
     constructor(app){
         this.app = app;
     }
 
     onShow() {
-        // Anzuzeigende HTML-Elemente ermitteln
-        let section = document.querySelector("#overview").cloneNode(true);
+        //create Container from imported HTML
+        let container = document.createElement("div");
+        container.innerHTML = overview.trim();
 
+        // Anzuzeigende HTML-Elemente ermitteln
+        let section = container.querySelector("#overview").cloneNode(true);
         let content = {
             className: "overview",
             main: section.querySelectorAll("main > *"),
